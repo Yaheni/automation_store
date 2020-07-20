c0480070d60d6f531d24c4f8c9a1496a367796ce
Yaheni <yaheni.sakharuk@gmail.com>
2020-07-20 11:15:52 +0300

    screen

--- src/main/java/PageObject/WomenPage.java
+++ src/main/java/PageObject/WomenPage.java
@@ -4,16 +4,13 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 
-import io.qameta.allure.Description;
-
-
 public class WomenPage {
 
      SelenideElement Blouse = Selenide.$("#center_column > ul > li:nth-child(2) > div > div.left-block > div > a.product_img_link");
      SelenideElement AddBlouseButton = Selenide.$x("//a[@data-id-product=\"2\"][@title=\"Add to cart\"]");
      SelenideElement ContinueShoppingButton = Selenide.$x("//span[@title=\"Continue shopping\"]");
     public void addBlouseToCard() {
-        Blouse.shouldBe(Condition.visible).scrollTo();
+        Blouse.shouldBe(Condition.visible).scrollTo().hover();
         AddBlouseButton.shouldBe(Condition.visible).click();
         ContinueShoppingButton.shouldBe(Condition.visible).click();
     }
